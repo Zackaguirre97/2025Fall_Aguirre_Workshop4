f2f294fbd15fe8d5592bbfbd459e0e51cbd2a50b
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-10-23 18:30:15 -0500

    Documented Vehicle class description (concerns/responsibilities)

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+/*
+* Vehicle contains the following fields:
+* - vin
+* - year
+* - make
+* - model
+* - vehicleType
+* - color
+* - odometer
+* - price.
+* Vehicle MUST be created with all fields.
+* Vehicle contains all relevant data about and represents a single vehicle.
+* */
 public class Vehicle {
+
 }

96449b8bbd877cbcb5b59b39f0dc769b43f090ed
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-10-23 16:22:39 -0500

    Updated class description comments to give more context about the list

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 /*
 * Dealership contains the following fields: name, address, and phone number.
-* Dealership contains a list of all the vehicles on its lot.
+* Dealership contains a list of all the vehicles on its lot (vehicles in the file).
 * Dealership MUST be created with all fields aside from the list of vehicles.
 * Dealership can return multiple filtered lists based on different search conditions.
 * Dealership can return a list of all its vehicles.

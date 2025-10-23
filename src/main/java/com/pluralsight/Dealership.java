e3fd365ca28d9eeba0f2653502603d339444b3b7
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-10-23 14:59:28 -0500

    Added Getters and Setters for the required properties

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -27,4 +27,29 @@ public class Dealership {
 
         vehicleList  = new ArrayList<>();
     }
+
+    // *** Getters/Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }

88abde2c275145d32f2ea2df5deb99f0503ce3b4
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-10-24 16:18:01 -0500

    Documented DealershipFileManager class description (concerns/responsibilities)

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+* DealershipFileManager is responsible for all file operations for the program.
+* DealershipFileManager contains the following methods:
+* - getDealership(): Dealership
+*   - can create a new Dealership object by reading a file.
+* - saveDealership(dealership)
+*   - can save a passed dealership object to a file.
+* */
 public class DealershipFileManager {
+
 }

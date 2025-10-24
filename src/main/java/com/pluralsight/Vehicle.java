10a91db86979c61b9e8f1578fd1871a0fb3554a7
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-10-23 21:28:43 -0500

    Added a constructor to Vehicle that takes all properties as passed parameters

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -25,4 +25,19 @@ public class Vehicle {
     private String color;
     private int odometer;
     private double price;
+
+    /*
+     * *** Const ***
+     */
+    // All args
+    public Vehicle(String vin, String year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
 }

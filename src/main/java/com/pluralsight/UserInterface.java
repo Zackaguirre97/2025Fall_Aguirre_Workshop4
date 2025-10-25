24b4d32694df862eea43f580186f4e9de7903672
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-10-25 18:31:26 -0500

    Documented UserInterface class description (concerns/responsibilities)

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+/*
+* UserInterface contains the following fields:
+* - Dealership
+* UserInterface contains the following methods:
+* - display()
+* - processGetByPriceRequest()
+* - processGetByMakeModelRequest()
+* - processGetByYearRequest()
+* - processGetByColorRequest()
+* - processGetByMileageRequest()
+* - processGetByVehicleTypeRequest()
+* - processGetAllVehiclesRequest()
+* - processAddVehicleRequest()
+* - processRemoveVehicleRequest()
+* UserInterface handles user interactions and executing the requested processes.
+*/
 public class UserInterface {
 }

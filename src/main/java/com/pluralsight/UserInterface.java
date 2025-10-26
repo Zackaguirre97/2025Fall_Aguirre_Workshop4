99b98d20bff912cad1cd00e5e9144b1e6900b864
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-10-25 21:29:51 -0500

    Created a method 'mainMenu()' to print the main menu to the console

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -45,6 +45,22 @@ public class UserInterface {
         this.dealership = DealershipFileManager.getDealership();
     }
 
+    // Print the main menu to the console
+    private void mainMenu() {
+        System.out.println("========== Dealership Menu ==========");
+        System.out.println("1. Find vehicle within a price range");
+        System.out.println("2. Find vehicles by make / model");
+        System.out.println("3. Find vehicles by year range");
+        System.out.println("4. Find vehicles by color");
+        System.out.println("5. Find vehicles by mileage range");
+        System.out.println("6. Find vehicles by type (car, truck, SUV, van)");
+        System.out.println("7. List ALL vehicles");
+        System.out.println("8. Add a vehicle");
+        System.out.println("9. Remove a vehicle");
+        System.out.println("99. Quit");
+        System.out.println("Select an option: ");
+    }
+
     // Handle the user requests to get a list of vehicles by price
     private void processGetByPriceRequest() {
 

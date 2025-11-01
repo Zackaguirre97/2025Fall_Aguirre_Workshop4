1c7f01240b0a77dae03f8b7316c447127180c62f
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-11-01 14:16:34 -0500

    feat(LeaseContract): add lease contract with monthly payment, lease fee, and expected ending value calculations

---
+++ src/main/java/com/pluralsight/contracts/LeaseContract.java
@@ -0,0 +1,74 @@
+package com.pluralsight.contracts;
+
+import com.pluralsight.models.Vehicle;
+
+
+public class LeaseContract extends Contract{
+    /*
+    * *** Properties/Fields ***
+    * */
+    private double expectedEndingValue;
+    private double leaseFee;
+    private Vehicle vehicle;
+    public static final int TERM = 36;
+    /*
+     * *** Constructor ***
+     * */
+    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        super(date, customerName, customerEmail, false);
+        this.vehicle = vehicle;
+        this.expectedEndingValue = calculateExpectedEndingValue();
+        this.leaseFee = calculateLeaseFee();
+    }
+    /*
+     * *** Getters/Setters ***
+     * */
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public void setExpectedEndingValue(double expectedEndingValue) {
+        this.expectedEndingValue = expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    public void setLeaseFee(double leaseFee) {
+        this.leaseFee = leaseFee;
+    }
+    /*
+     * *** Methods ***
+     * */
+    public double calculateExpectedEndingValue() {
+        return vehicle.getPrice() * .5;
+    }
+
+    public double calculateLeaseFee() {
+        return vehicle.getPrice() * .07;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return getMonthlyPayment() * TERM;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        double price = vehicle.getPrice();
+        double rate = .04 / 12;
+        double baseAmount = (price - expectedEndingValue) + leaseFee;
+        return (baseAmount * rate) / (1 - Math.pow(1 + rate, -TERM));
+    }
+
+    @Override
+    public String toString() {
+        return "LeaseContract{" +
+                "expectedEndingValue=" + expectedEndingValue +
+                ", leaseFee=" + leaseFee +
+                ", monthlyPayment=" + getMonthlyPayment() +
+                ", TotalPrice=" + getTotalPrice() +
+                '}';
+    }
+}
